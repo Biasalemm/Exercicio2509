85045750b41a018f7bc5f033005e2456b99ec655
Biasalemm <159277371+Biasalemm@users.noreply.github.com>
2024-09-25 16:40:51 -0300

    primeiro commit

---
+++ Animal.java
@@ -0,0 +1,52 @@
+package Exercicios2509;
+
+class Animal {
+    private String nome;
+    private String cor;
+    private String tamanho;
+    private String peso;
+    private String raca;
+    
+    protected String getnome() {
+    	return nome;
+    }
+    protected void setnome (String nome) {
+      this.nome = nome;
+    }
+    protected String getcor() {
+    	return cor;
+    }
+    protected void setcor (String cor) {
+      this.cor = cor;
+    }
+    protected String gettamanho() {
+    	return tamanho;
+    }
+    protected void settamanho (String tamanho) {
+      this.tamanho = tamanho;
+    }
+    protected String getpeso() {
+    	return peso;
+    }
+    protected void setpeso (String peso) {
+      this.peso = peso;
+    }
+    protected String getraca() {
+    	return raca;
+    }
+    protected void setraca (String raca) {
+      this.raca = raca;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
